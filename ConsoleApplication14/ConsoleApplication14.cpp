4f0ea7073131d7a7894720e0ffe23ab0de178e18
apssd7696-byte <apssd7696@gmail.com>
2026-05-04 22:43:04 +0300

    Update ConsoleApplication14.cpp

--- ConsoleApplication14/ConsoleApplication14.cpp
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -1,22 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "");
+    cout << fixed << setprecision(4);
     double x = 2.0;
     double sum = 0.0;
-    double term = -1.0 / (2.0 * 4.0);
-    double fact = 2.0;
+    double term;
+    double fact = 1.0;
     double x_pow = 1.0;
-    int i = 1;
+    int i = 0;
 
-    while (fabs(term) > 1e-12) {
+    while (true) {
+        i++;
+        if (i == 1) fact = 2.0;
+        else fact *= (2 * i - 1) * (2 * i);
+        if (i > 1) x_pow *= x * x;
+        term = (pow(-1,i)*x_pow)/(fact*(2*i+2));
+        if (fabs(term) < 1e-12) break;
         sum += term;
-        ++i;
-        fact *= (2 * i - 1) * (2 * i);
-        x_pow *= x * x;
-        term = ((i % 2 == 0) ? 1.0 : -1.0) * x_pow / (fact * (2 * i + 2));
     }
-
-    std::cout << "c) Sum = " << sum << std::endl;
-    return 0;
+    cout << "Результат:" << sum << endl;
 }
